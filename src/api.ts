afbb34b87e7748cebea52cabc3efe6225d4c89c0
Or Shaviv <shavivor@gmail.com>
2024-01-24 13:04:28 +0200

    remove mock

--- src/api.ts
+++ src/api.ts
@@ -21,39 +21,39 @@ interface SearchCitiesResponse {
 export const searchCities = async (
   city: string
 ): Promise<SearchCitiesResponse> => {
-  // TODO: Remove mock
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // // TODO: Remove mock
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return {
-    cities: [
-      {
-        key: "215854",
-        type: "city",
-        localizedName: "Tel Aviv",
-        country: {
-          id: "IL",
-          localizedName: "Israel",
-        },
-        administrativeArea: {
-          id: "TA",
-          localizedName: "Tel Aviv",
-        },
-      },
-      {
-        key: "123345",
-        type: "city",
-        localizedName: "New York",
-        country: {
-          id: "US",
-          localizedName: "United States",
-        },
-        administrativeArea: {
-          id: "NY",
-          localizedName: "New York",
-        },
-      },
-    ],
-  };
+  // return {
+  //   cities: [
+  //     {
+  //       key: "215854",
+  //       type: "city",
+  //       localizedName: "Tel Aviv",
+  //       country: {
+  //         id: "IL",
+  //         localizedName: "Israel",
+  //       },
+  //       administrativeArea: {
+  //         id: "TA",
+  //         localizedName: "Tel Aviv",
+  //       },
+  //     },
+  //     {
+  //       key: "123345",
+  //       type: "city",
+  //       localizedName: "New York",
+  //       country: {
+  //         id: "US",
+  //         localizedName: "United States",
+  //       },
+  //       administrativeArea: {
+  //         id: "NY",
+  //         localizedName: "New York",
+  //       },
+  //     },
+  //   ],
+  // };
 
   const res = await fetch(
     `${baseURL}/search-city?q=${encodeURIComponent(city)}`
@@ -80,63 +80,63 @@ export interface Forecast {
 export const forecast5d = async (
   cityKey: string
 ): Promise<{ forecast: Forecast[] }> => {
-  // TODO: Remove mock
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // // TODO: Remove mock
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return {
-    forecast: [
-      {
-        name: "Mosquito Activity Forecast",
-        localDateTime: "2024-01-21T07:00:00-05:00",
-        epochDateTime: 1705838400,
-        value: 0,
-        category: "Low",
-        mobileLink:
-          "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-        link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-      },
-      {
-        name: "Mosquito Activity Forecast",
-        localDateTime: "2024-01-22T07:00:00-05:00",
-        epochDateTime: 1705924800,
-        value: 1,
-        category: "Low",
-        mobileLink:
-          "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-        link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-      },
-      {
-        name: "Mosquito Activity Forecast",
-        localDateTime: "2024-01-23T07:00:00-05:00",
-        epochDateTime: 1706011200,
-        value: 2,
-        category: "Moderate",
-        mobileLink:
-          "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-        link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-      },
-      {
-        name: "Mosquito Activity Forecast",
-        localDateTime: "2024-01-24T07:00:00-05:00",
-        epochDateTime: 1706097600,
-        value: 3,
-        category: "High",
-        mobileLink:
-          "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-        link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-      },
-      {
-        name: "Mosquito Activity Forecast",
-        localDateTime: "2024-01-25T07:00:00-05:00",
-        epochDateTime: 1706184000,
-        value: 4,
-        category: "High",
-        mobileLink:
-          "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-        link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
-      },
-    ],
-  };
+  // return {
+  //   forecast: [
+  //     {
+  //       name: "Mosquito Activity Forecast",
+  //       localDateTime: "2024-01-21T07:00:00-05:00",
+  //       epochDateTime: 1705838400,
+  //       value: 0,
+  //       category: "Low",
+  //       mobileLink:
+  //         "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //       link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //     },
+  //     {
+  //       name: "Mosquito Activity Forecast",
+  //       localDateTime: "2024-01-22T07:00:00-05:00",
+  //       epochDateTime: 1705924800,
+  //       value: 1,
+  //       category: "Low",
+  //       mobileLink:
+  //         "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //       link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //     },
+  //     {
+  //       name: "Mosquito Activity Forecast",
+  //       localDateTime: "2024-01-23T07:00:00-05:00",
+  //       epochDateTime: 1706011200,
+  //       value: 2,
+  //       category: "Moderate",
+  //       mobileLink:
+  //         "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //       link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //     },
+  //     {
+  //       name: "Mosquito Activity Forecast",
+  //       localDateTime: "2024-01-24T07:00:00-05:00",
+  //       epochDateTime: 1706097600,
+  //       value: 3,
+  //       category: "High",
+  //       mobileLink:
+  //         "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //       link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //     },
+  //     {
+  //       name: "Mosquito Activity Forecast",
+  //       localDateTime: "2024-01-25T07:00:00-05:00",
+  //       epochDateTime: 1706184000,
+  //       value: 4,
+  //       category: "High",
+  //       mobileLink:
+  //         "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //       link: "http://www.accuweather.com/en/us/new-york-ny/10021/mosquito-activity-weather/349727?lang=en-us",
+  //     },
+  //   ],
+  // };
 
   const res = await fetch(
     `${baseURL}//forecast/5d/${encodeURIComponent(cityKey)}`
